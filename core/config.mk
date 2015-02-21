AOSPA/android_build

core/config.mk: Correct possible issue with sepolicy inclusion
It is possible that the version of make used on the build
servers has issues with the eval command.  eval shouldn't
even be needed in this case, so we can just do a straight
include

Change-Id: I255890b6788a68e107836a9dfb90f03e39846edf
Signed-off-by: Evan Anderson <evananderson@aospa.co>

core/config.mk
@@ -621,6 +621,6 @@ include $(BUILD_SYSTEM)/qcom_target.mk
 
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/pa/sepolicy/sepolicy.mk)
+include $(TOPDIR)vendor/pa/sepolicy/sepolicy.mk
 
 include $(BUILD_SYSTEM)/dumpvar.mk

humor2008/android_build-2

core/config.mk
@@ -624,7 +624,7 @@ include $(BUILD_SYSTEM)/qcom_target.mk
 
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/screwd/sepolicy/sepolicy.mk)
+include $(TOPDIR)vendor/screwd/sepolicy/sepolicy.mk
 ## endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk